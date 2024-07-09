8df9c2a0185fe0c071064cf4acd1bc68642f7588
Girish Watwani <girishwatwani@Girishs-MacBook-Air.local>
2024-07-09 20:59:02 +0400

    Added 3 linked list insert Methods - Beginning, End and at random place

--- LinkedList/linkedlist.js
+++ LinkedList/linkedlist.js
@@ -1,12 +1,35 @@
 class Node {
-    constructor(data, next=null) {
-        this.data = data;
-        this.next = next
-    }
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
 }
 
 class LinkedList {
-    constructor() {
-        this.head = null;
-    }
+  constructor() {
+    this.head = null;
+  }
+}
+
+LinkedList.prototype.insertAtBeginning = function (data) {
+  const newNode = new Node(data);
+  this.head = newNode;
+};
+
+LinkedList.prototype.insertAtEnd = function (data) {
+  const newNode = new Node(data);
+  if (!this.head) {
+    this.head = newNode;
+    return;
+  }
+  let last = this.head;
+  while(last.next) {
+    last = last.next;
+  }
+  last.next = newNode;
+};
+
+LinkedList.prototype.insertAfter = function(prevNode, data) {
+    const newNode = new Node(data, prevNode.next);
+    prevNode.next = newNode;
 }
